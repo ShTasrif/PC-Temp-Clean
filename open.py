cb7afccd18810ce026ac11eb6df184415aba922d
CYBER SH <85736436+ShTasrif@users.noreply.github.com>
2023-01-28 00:08:01 -0800

    Add files via upload

---
+++ open.py
@@ -0,0 +1,40 @@
+import os
+import time
+
+
+
+rst="\033[0m"
+b="\033[1;30m"
+r="\033[1;31m"
+g="\033[1;32m"
+yl="\033[1;33m"
+bl="\033[1;34m"
+p="\033[1;35m"
+c="\033[1;36m"
+w="\033[1;37m"
+
+
+
+print(""""
+      ████████╗░█████╗░░██████╗██████╗░██╗███████╗
+      ╚══██╔══╝██╔══██╗██╔════╝██╔══██╗██║██╔════╝
+      ░░░██║░░░███████║╚█████╗░██████╔╝██║█████╗░░
+      ░░░██║░░░██╔══██║░╚═══██╗██╔══██╗██║██╔══╝░░
+      ░░░██║░░░██║░░██║██████╔╝██║░░██║██║██║░░░░░
+      ░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░""")
+print("""   ╔═══════════════════════════════════════════════╗
+   ║       [✓] TOOL NAME : PC TEMP FILES CLEANER   ║ 
+   ║       [✓] YOUTUBE : CYBER SH                  ║
+   ║       [✓] GITHUB : SH TASRIF                  ║
+   ║       [✓] FACEBOOK : H.CYBERSH                ║
+   ║       [✓] TELEGRAM : TASRIF_HOSSEN_SHUVO      ║
+   ║       [✓] INSTAGRAM : TASRIF.HOSSEN.SHUVO     ║
+   ║       [✓] EMAIL : CYBERSHBD@GMAIL.COM         ║
+   ╚═══════════════════════════════════════════════╝""")
+
+
+os.startfile(r'C:\Users\TASRIF~1\AppData\Local\Temp')
+
+os.startfile(r'C:\Windows\Temp')
+
+time.sleep(10)
